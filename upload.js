2484fef3691424e95d6b1b9c6d5d2c86877698ed
Hector Benitez <hector.benitez.fletes@gmail.com>
2020-04-27 18:59:59 -0500

    sample upload

---
+++ upload.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const AWS = require('aws-sdk')
+
+const ID = '';
+const SECRET = '';
+const BUCKET_NAME = 'talent-platform-bucket';
+
+const s3 = new AWS.S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET
+});
+
+const uploadFile = (fileName) => {
+  // Read content from the file
+  const fileContent = fs.readFileSync(fileName);
+
+  // Setting up S3 upload parameters
+  const params = {
+      Bucket: BUCKET_NAME,
+      Key: 'sample.jpg', // File name you want to save as in S3
+      Body: fileContent
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function(err, data) {
+      if (err) {
+          throw err;
+      }
+      console.log(`File uploaded successfully. ${data.Location}`);
+  });
+};
+
+uploadFile('sample.jpg');
